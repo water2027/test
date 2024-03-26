f049205556a2b5000db1b21b14b8b9774c0e6dcf
water2027 <136900643+water2027@users.noreply.github.com>
2024-03-26 14:54:56 +0800

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('resize', function() {
-    document.querySelector('a-scene').style.width = window.innerWidth + 'px';
-    document.querySelector('a-scene').style.height = window.innerHeight + 'px';
-});
